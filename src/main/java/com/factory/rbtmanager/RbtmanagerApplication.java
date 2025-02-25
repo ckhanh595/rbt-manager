c897857b4c5970147182009fcfcb0e742a72f87e
ckhanh595 <ckhanh595@gmail.com>
2025-02-25 14:04:00 +0700

    Initial commit

---
+++ src/main/java/com/factory/rbtmanager/RbtmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.factory.rbtmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbtmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbtmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factory/rbtmanager/RbtmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.rbtmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbtmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
